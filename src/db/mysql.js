d3ba62131e19b57be174507dd3055903f88baa39
jiaoruogu <ruogu.jiao@outlook.com>
2021-06-07 11:27:32 +0800

    use pool to connect mysql

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,26 +1,49 @@
-const { createConnection } = require('mysql')
+const { createConnection, createPool } = require('mysql')
 const {MYSQL_CONF} = require('../conf/db')
 
-const con = createConnection(MYSQL_CONF)
+// const con = createConnection(MYSQL_CONF)
 
 
-con.connect()
+// con.connect()
+
+// function exec(sql) {
+
+//   return new Promise((resolve, reject) => {
+//     con.query(sql, (err, result) => {
+//       if (err) {
+//         console.error(err);
+//         reject(err)
+//         return
+//       }
+
+//       resolve(result)
+//     })
+//   })
+// }
+
+
+// 连接池的形式连接
+const pool = createPool({
+  connectionLimit: 10,
+  ...MYSQL_CONF
+})
 
 function exec(sql) {
-
   return new Promise((resolve, reject) => {
-    con.query(sql, (err, result) => {
-      if (err) {
-        console.error(err);
+    pool.getConnection((err, con) => {
+      if(err) {
         reject(err)
         return
       }
+      con.query(sql, (error, result) => {
+        con.release()
+        resolve(result)
+        if (error) reject(error)
 
-      resolve(result)
+      })
     })
-  })
+  });
 }
-
 module.exports = {
   exec,
 }
